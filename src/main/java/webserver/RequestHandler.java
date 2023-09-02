86d68ef3551e1af96f2e4ba493a9fcef04276177
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-03 00:58:34 +0900

    feat: 1단계 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,16 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
 
   private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+  private static final String PREFIX_PATH = "./webapp";
 
   private final Socket connection;
 
@@ -18,15 +23,28 @@ public class RequestHandler extends Thread {
     this.connection = connectionSocket;
   }
 
+  private static String[] readAndTokenize(final BufferedReader br) throws IOException {
+    final String line = br.readLine();
+    return line.split(" ");
+  }
+
   @Override
   public void run() {
     log.debug("New Client Connect! Connected IP : {}, Port : {}", this.connection.getInetAddress(),
         this.connection.getPort());
 
-    try (final InputStream in = this.connection.getInputStream(); final OutputStream out = this.connection.getOutputStream()) {
+    try (
+        final InputStream in = this.connection.getInputStream();
+        final OutputStream out = this.connection.getOutputStream();
+        final InputStreamReader reader = new InputStreamReader(in);
+        final BufferedReader br = new BufferedReader(reader)
+    ) {
       // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+      final String[] tokens = readAndTokenize(br);
+      final String resourcePath = tokens[1];
+
       final DataOutputStream dos = new DataOutputStream(out);
-      final byte[] body = "Hello World".getBytes();
+      final byte[] body = Files.readAllBytes(new File(PREFIX_PATH + resourcePath).toPath());
       response200Header(dos, body.length);
       responseBody(dos, body);
     } catch (final IOException e) {
