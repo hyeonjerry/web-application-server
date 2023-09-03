3dbd16b5be12560221c716674a786c89ddd773ec
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-09-03 13:53:37 +0900

    feat: 요구사항 4 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -57,6 +57,7 @@ public class RequestHandler extends Thread {
         headers.put(pair.getKey(), pair);
       }
 
+      final DataOutputStream dos = new DataOutputStream(out);
       if (httpMethod.equalsIgnoreCase("post")) {
         final Pair pair = headers.get("Content-Length");
         final int contentLength = Integer.parseInt(pair.getValue());
@@ -69,12 +70,12 @@ public class RequestHandler extends Thread {
             values.get("email")
         );
         System.out.println(user);
+        response302Header(dos, "/index.html");
+      } else {
+        final byte[] body = Files.readAllBytes(new File(PREFIX_PATH + resourcePath).toPath());
+        response200Header(dos, body.length);
+        responseBody(dos, body);
       }
-
-      final DataOutputStream dos = new DataOutputStream(out);
-      final byte[] body = Files.readAllBytes(new File(PREFIX_PATH + resourcePath).toPath());
-      response200Header(dos, body.length);
-      responseBody(dos, body);
     } catch (final IOException e) {
       log.error(e.getMessage());
     }
@@ -85,6 +86,16 @@ public class RequestHandler extends Thread {
       dos.writeBytes("HTTP/1.1 200 OK \r\n");
       dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
       dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+      dos.writeBytes("\r\n");
+    } catch (final IOException e) {
+      log.error(e.getMessage());
+    }
+  }
+
+  private void response302Header(final DataOutputStream dos, final String location) {
+    try {
+      dos.writeBytes("HTTP/1.1 302 FOUND \r\n");
+      dos.writeBytes("Location: " + location + "\r\n");
       dos.writeBytes("\r\n");
     } catch (final IOException e) {
       log.error(e.getMessage());
